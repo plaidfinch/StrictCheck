9673aff5cc44b7a096d7ef403026a714db50e5fa
Leonidas Lampropoulos <lemonidas_13@hotmail.com>
2017-11-20 13:32:18 -0500

    Playing around with arbitrary, coarbitrary and continuations :)

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances, DeriveGeneric, UndecidableInstances #-}
 import LazyCoArbitrary
 
 import GHC.Generics
@@ -7,6 +7,37 @@ import Test.QuickCheck
 data MyList a = MyNil | MyCons a (MyList a)
   deriving (Show, Generic)
 
+class ArbitraryK1 a where
+  arbitraryK1 :: Gen a -> Gen a
+
+instance Arbitrary a => ArbitraryK1 [a] where
+  arbitraryK1 g =
+    -- No base case! 
+    frequency [ (1, (:) <$> arbitrary <*> g ) ]
+
+class ArbitraryK a where
+  arbitraryK :: Gen a -> Gen a 
+
+instance ArbitraryK1 a => ArbitraryK a where
+  -- | TODO: Weights towards g?
+  arbitraryK g = oneof [ g , arbitraryK1 (arbitraryK g) ]
+
+class CoArbitraryK a where
+  coarbitraryK :: ArbitraryK b => a -> Gen b -> Gen b
+
+instance (CoArbitraryK a, CoArbitraryK b) => CoArbitraryK (a,b) where
+  coarbitraryK t g = do
+    arbitraryK $ frequency [ (1, variant 0 g)
+                           , (1, let (x,y) = t in
+                                 coarbitraryK x $ coarbitraryK y g ) ]
+
+instance (CoArbitraryK a, ArbitraryK [a]) => CoArbitraryK [a] where
+  coarbitraryK l g = do
+    arbitraryK $ frequency [ (1, variant 0 g)
+                           , (1, case l of
+                                 [] -> variant 1 g
+                                 x : xs -> variant 2 $ coarbitraryK (x,xs) g ) ]
+      
 -- | Annoying boilerplate line
 instance LazyCoArbitrary a => LazyCoArbitrary (MyList a)
 
@@ -23,5 +54,5 @@ testProp f l = (f . LazyCoarb) l == (f . LazyCoarb) l
 instance Show (LazyCoarb (MyList Int) -> Int) where
   show f = "<function>"
 
-main =
+main = do
   quickCheck testProp
