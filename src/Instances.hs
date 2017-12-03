0a64c9b2c984fc1767d195df5f5ace8fcb3babdf
Kenny Foner <kwf@very.science>
2017-12-03 00:15:32 -0500

    Add instances for Data.Tree, to test branching generation

--- src/Instances.hs
+++ src/Instances.hs
@@ -6,6 +6,8 @@ module Instances where
 import Generate
 import Test.QuickCheck
 
+import Data.Tree
+
 instance Produce Integer where
   produce = produceArbitrary
 
@@ -24,7 +26,7 @@ instance (Consume a, Consume b) => Consume (a, b) where
 instance (Produce a) => Produce [a] where
   produce inputs = do
     frequency [ (1, return [])
-              , (4, (:) <$> recur inputs
+              , (1, (:) <$> recur inputs
                         <*> recur inputs)
               ]
 
@@ -33,3 +35,13 @@ instance (Consume a) => Consume [a] where
   consume (x : xs) = fields [ (1, consume x)
                             , (1, consume xs)
                             ]
+
+instance (Consume a) => Consume (Tree a) where
+  consume (Node r cs) = fields [ (1, consume r)
+                               , (1, consume cs) ]
+
+instance (Produce a) => Produce (Tree a) where
+  produce input =
+    Node <$> recur input
+         <*> frequency [ (1, return [])
+                       , (2, recur input) ]
