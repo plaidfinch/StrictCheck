020d4763b26c8e9e19bfad86718f29b37efd02b0
Kenny Foner <kwf@very.science>
2018-02-08 01:04:53 -0500

    [temp] More helpful exploration tools in the main StrictCheck file

--- src/Test/StrictCheck.hs
+++ src/Test/StrictCheck.hs
@@ -2,16 +2,17 @@
 {-# OPTIONS_GHC -fno-warn-dodgy-exports -fno-warn-unused-imports #-}
 
 module Test.StrictCheck
-  ( module Test.StrictCheck.Curry
-  , module Test.StrictCheck.Produce
-  , module Test.StrictCheck.Consume
-  , module Test.StrictCheck.Observe
-  , module Test.StrictCheck.Instances
-  , module Test.StrictCheck.Demands
+  -- ( module Test.StrictCheck.Curry
+  -- , module Test.StrictCheck.Produce
+  -- , module Test.StrictCheck.Consume
+  -- , module Test.StrictCheck.Observe
+  -- , module Test.StrictCheck.Instances
+  -- , module Test.StrictCheck.Demands
 
-  , module Generics.SOP
-  , module Generics.SOP.NP
-  ) where
+  -- , module Generics.SOP
+  -- , module Generics.SOP.NP
+  -- )
+  where
 
 
 import Test.StrictCheck.Curry
@@ -23,6 +24,7 @@ import Test.StrictCheck.Demands
 
 import Generics.SOP
 import Generics.SOP.NP
+import qualified GHC.Generics as GHC
 
 import Test.QuickCheck
 import Data.List
@@ -44,3 +46,33 @@ withGrid x y = do
   putStrLn ""
   mapM_ print (transpose results)
   return f
+
+data Binary =
+  N Binary Binary | L
+  deriving stock (Eq, Ord, Show, GHC.Generic)
+  deriving anyclass (Generic, HasDatatypeInfo, Consume, Observe)
+
+binary :: Int -> Binary
+binary 0 = L
+binary n = N (binary (n - 1)) (binary (n - 1))
+
+data Omega = Succ Omega | Zero
+  deriving stock (Eq, Ord, Show, GHC.Generic)
+  deriving anyclass (Generic, HasDatatypeInfo, Consume, Observe)
+
+instance Produce Omega where
+  produce input = Succ <$> recur input
+
+omega :: Omega
+omega = Succ omega
+
+treeToOmega :: IO (Binary -> Omega)
+treeToOmega = generate (freely produce)
+
+forceOmegaN :: Int -> Omega -> ()
+forceOmegaN 0 _        = ()
+forceOmegaN _    Zero  = ()
+forceOmegaN n (Succ o) = forceOmegaN (n - 1) o
+
+observeTreeToOmega :: (Binary -> Omega) -> Int -> Int -> Field Thunk Binary
+observeTreeToOmega f m n = snd $ observe1 (forceOmegaN n) f (binary m)
