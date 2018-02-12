742bdf1e489200e57e96d758b09a74925c75c0d5
hengchu <hengchu.zhang@gmail.com>
2018-02-11 21:15:06 -0500

    tests/Tests.hs: added a test case for rotate from Okasaki queues

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -20,6 +20,9 @@ import Data.List
 testList :: [Integer]
 testList = [1,2,3,4,5,6]
 
+testList2 :: [Integer]
+testList2 = [7,8,9,10,11]
+
 whnfContext :: a -> ()
 whnfContext = flip seq ()
 
@@ -29,9 +32,22 @@ testReverse =
   spineStrict testList ~=? dIn
   where (_ {-dOut-}, dIn) = observe1 whnfContext reverse testList
 
+-- rotate (from Okasaki Queues) should be whnf strict on whnf
+rotate :: [a] -> [a] -> [a] -> [a]
+rotate []            []  as =                       as
+rotate []       (b : bs) as =     rotate [] bs (b : as)
+rotate (f : fs)      []  as = f : rotate fs []      as
+rotate (f : fs) (b : bs) as = f : rotate fs bs (b : as)
+
+testRotate :: Test
+testRotate =
+  whnf testList ~=? dIn
+  where (_ {-dOut-}, dIn) = observe1 whnfContext (\fs -> rotate fs testList2 []) testList
+
 tests :: Test
 tests = TestList [
     TestLabel "Data.List reverse" testReverse
+  , TestLabel "Data.List rotate"  testRotate
   ]
 
 main :: IO ()
