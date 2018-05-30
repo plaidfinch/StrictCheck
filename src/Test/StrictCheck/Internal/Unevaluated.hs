b3733828eeea6437c8c5e2ddb4b18d3caa63a9a1
Kenny Foner <kwf@very.science>
2018-05-30 17:27:14 -0400

    [doc] Document internal Unevaluated exception module

--- src/Test/StrictCheck/Internal/Unevaluated.hs
+++ src/Test/StrictCheck/Internal/Unevaluated.hs
@@ -1,7 +1,20 @@
-module Test.StrictCheck.Internal.Unevaluated where
+{-| This module defines the internal exception type used to implement the
+    to/from-Demand methods in "Test.StrictCheck.Demand". We don't export this
+    type from the library to discourage users from interacting with this
+    mechanism.
+-}
+
+module Test.StrictCheck.Internal.Unevaluated
+  ( Unevaluated(..)
+  ) where
 
 import Control.Exception
 
-data Unevaluated = Unevaluated deriving Show
+-- | In @fromDemand@, this exception is (purely, lazily) thrown whenever a
+-- @Thunk@ is encountered. In @toDemand@, it is caught and converted back to a
+-- @Thunk@.
+data Unevaluated
+  = Unevaluated
+  deriving Show
 
 instance Exception Unevaluated
